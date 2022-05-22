010f06ef0c80770fd8fd1ab998a9b489b12d5da4
replicate <replicate@DESKTOP-J7M1L13>
2022-05-22 16:39:11 +0300

    Добавьте файлы проекта.

---
+++ manukhin18/manukhin18/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace manukhin18
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+
+            //string url = "http://www.boredapi.com/api/activity/";
+
+            try
+            {
+                HttpClient client = new HttpClient();
+                HttpResponseMessage response = await client.GetAsync($"https://www.boredapi.com/api/activity");
+                HttpContent responseContent = response.Content;
+                string json = await responseContent.ReadAsStringAsync();
+                var root = JsonConvert.DeserializeObject<Root>(json);
+
+                lable.TextColor = Color.Black;
+                lable.Text = "Деятельность: " + root.activity + "\r\n" +
+                             "Тип: " + root.type + "\r\n" +
+                             "Участники: " + root.participants + "\r\n" +
+                             "Цена: " + root.price + "\r\n" +
+                             "Ссылка: " + root.link + "\r\n" +
+                             "Ключ: " + root.key + "\r\n" +
+                             "Доступность: " + root.accessibility;
+
+            }
+            catch (Exception ex)
+            {
+                lable.TextColor = Color.Red;
+                lable.Text = ex.ToString();
+            }
+
+        }
+    }
+}
+
---
+++ manukhin18/manukhin18/Root.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace manukhin18
+{
+    public class Root
+    {
+        public string activity { get; set; }
+        public string type { get; set; }
+        public double participants { get; set; }
+        public double price { get; set; }
+        public string link { get; set; }
+        public string key { get; set; }
+        public double accessibility { get; set; }
+    }
+
+}
